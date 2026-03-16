9d3c3239e04264aecd249d074bc14558d206fb9f
Braxton Ward <braxton.ward@atomicfi.com>
2026-03-16 14:20:52 -0600

    fix: update AtomicSDK version to 3.27.2 with vendored_frameworks

---
+++ ios/frameworks/QuantumIOS.xcframework/ios-arm64_x86_64-simulator/QuantumIOS.framework/Headers/QuantumIOS.h
@@ -0,0 +1,16 @@
+//
+//  QuantumIOS.h
+//  QuantumIOS
+//
+//  Created by Erik Sargent on 8/24/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QuantumIOS.
+FOUNDATION_EXPORT double QuantumIOSVersionNumber;
+
+//! Project version string for QuantumIOS.
+FOUNDATION_EXPORT const unsigned char QuantumIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AtomicTransactiOS/PublicHeader.h>
